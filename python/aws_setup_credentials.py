593149f3c454d3e1c5d55b3f31f9c96e385ac0de
Hailong Shi <hailong.shi@earnin.com>
2020-05-05 07:43:25 -0400

    Add aws local credential setup script

---
+++ python/aws_setup_credentials.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.7
+import subprocess
+import os
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='MFA CODE')
+parser.add_argument('--code', '-c', type=str, help='MFA CODE')
+
+args = parser.parse_args();
+cmd = 'aws sts get-session-token --serial-number arn:aws:iam::429750608758:mfa/hailong.shi --profile earnin --token-code ' + args.code + ' > tmp.json'
+# print(cmd)
+p = subprocess.Popen(cmd, shell=True)
+p.wait()
+
+with open('tmp.json') as json_file:
+    data = json.load(json_file)
+
+id = 'aws_access_key_id = ' + data['Credentials']['AccessKeyId'] + '\n'
+key = 'aws_secret_access_key = ' + data['Credentials']['SecretAccessKey'] + '\n'
+token = 'aws_session_token = ' + data['Credentials']['SessionToken'] + '\n'
+
+with open('credentials','r') as input: 
+    lines = input.readlines()
+
+lines[1]=id
+lines[2]=key
+lines[3]=token
+with open('credentials','w') as output: 
+    output.writelines(lines)
+with open('Z:/.aws/credentials','w') as output: 
+    output.writelines(lines)
